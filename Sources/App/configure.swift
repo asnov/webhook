3970152ff3682a720ad306ded3edd80a26e0b1f5
Alex <alex@lpdev.pro>
2024-07-03 20:24:58 -0300

    added reading environment variables;

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,12 +2,15 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
+    let port = Int(Environment.get("PORT") ?? "8080")
+    let hostname = Environment.get("HOSTNAME") ?? "localhost"
+
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     // register routes
 
     app.commands.use(HelloCommand(), as: "hola")
-    app.http.server.configuration.address = .hostname("localhost", port: 8080)
+    app.http.server.configuration.address = .hostname(hostname, port: port)
     app.http.server.configuration.serverName = "Apache"
     
     try routes(app)
